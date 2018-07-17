87b5412b9700660f0e668ee60b2fe56b69f90325
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-07-17 13:41:52 +0300

    fix

--- src/Expressions/BooleanOrExpression.php
+++ src/Expressions/BooleanOrExpression.php
@@ -5,7 +5,6 @@ namespace Interpreter\Expressions;
 use Interpreter\Abstracts\OperatorExpression;
 use Interpreter\Context\InterpreterContext;
 
-
 class BooleanOrExpression extends OperatorExpression
 {
     protected function doInterpret(InterpreterContext $context, $result_l, $result_r) {
